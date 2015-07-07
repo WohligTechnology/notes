2e24808ec683ae89affed39ecc2c18b7556a6d1e
vigkast <vigneshkasthuri2009@gmail.com>
2015-07-07 12:11:35 +0530

    URL

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-var adminurl = "http://localhost:1337/";
+var adminurl = "http://130.211.118.86/";
 var navigationservice = angular.module('navigationservice', [])
 
 .factory('NavigationService', function ($http) {
